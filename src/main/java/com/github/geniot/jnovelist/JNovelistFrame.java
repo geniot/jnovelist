e053105334dcd56310ffa708f20ac66b0cb45403
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2015-06-23 19:39:46 +0200

    Added undo/redo

--- src/main/java/com/github/geniot/jnovelist/JNovelistFrame.java
+++ src/main/java/com/github/geniot/jnovelist/JNovelistFrame.java
@@ -3,10 +3,16 @@ package com.github.geniot.jnovelist;
 import org.mapdb.DB;
 
 import javax.swing.*;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
 import javax.swing.text.Document;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.StyleSheet;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.net.URL;
@@ -68,6 +74,47 @@ public class JNovelistFrame extends JFrame {
         Document doc = cssKit.createDefaultDocument();
         editorPane.setDocument(doc);
         linePainter = new LinePainter(editorPane);
+
+        final UndoManager undo = new UndoManager();
+
+        // Listen for undo and redo events
+        doc.addUndoableEditListener(new UndoableEditListener() {
+            public void undoableEditHappened(UndoableEditEvent evt) {
+                undo.addEdit(evt.getEdit());
+            }
+        });
+
+        // Create an undo action and add it to the text component
+        editorPane.getActionMap().put("Undo",
+                new AbstractAction("Undo") {
+                    public void actionPerformed(ActionEvent evt) {
+                        try {
+                            if (undo.canUndo()) {
+                                undo.undo();
+                            }
+                        } catch (CannotUndoException e) {
+                        }
+                    }
+                });
+
+        // Bind the undo action to ctl-Z
+        editorPane.getInputMap().put(KeyStroke.getKeyStroke("control Z"), "Undo");
+
+        // Create a redo action and add it to the text component
+        editorPane.getActionMap().put("Redo",
+                new AbstractAction("Redo") {
+                    public void actionPerformed(ActionEvent evt) {
+                        try {
+                            if (undo.canRedo()) {
+                                undo.redo();
+                            }
+                        } catch (CannotRedoException e) {
+                        }
+                    }
+                });
+
+        // Bind the redo action to ctl-Y
+        editorPane.getInputMap().put(KeyStroke.getKeyStroke("control Y"), "Redo");
 
         scrollPane = new JScrollPane(editorPane);
 
