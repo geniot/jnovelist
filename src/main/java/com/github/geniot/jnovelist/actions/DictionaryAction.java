e041788cebc0647c59d117979e32751f9088e6c8
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2016-07-23 23:54:03 +0200

    spelling and synonyms

---
+++ src/main/java/com/github/geniot/jnovelist/actions/DictionaryAction.java
@@ -0,0 +1,9 @@
+package com.github.geniot.jnovelist.actions;
+
+/**
+ * Author: Vitaly Sazanovich
+ * Email: vitaly.sazanovich@gmail.com
+ * Date: 19/07/16
+ */
+public class DictionaryAction {
+}
---
+++ src/main/java/com/github/geniot/jnovelist/actions/InfoAction.java
@@ -0,0 +1,9 @@
+package com.github.geniot.jnovelist.actions;
+
+/**
+ * Author: Vitaly Sazanovich
+ * Email: vitaly.sazanovich@gmail.com
+ * Date: 20/07/16
+ */
+public class InfoAction {
+}
